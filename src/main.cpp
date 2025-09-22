bc8636992ed7f3cbf67355f18b034ee95da36a4e
rcc-navin-velazco <navin.velazco@rccd.edu>
2025-09-22 16:46:27 -0700

    Made sure to include function calls exercise 2 and exercise 3in main

--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,11 @@ int main(int argc, char* argv[])
     separator();
 
     std::cout << std::endl << "Exercise 2:" << std::endl;
+    exercise_2();
     separator();
 
     std::cout << std::endl << "Exercise 3:" << std::endl;
-
+    exercise_3();
     separator();
 
     
